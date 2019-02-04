ec11fae385728819cb5ee560eff6a4a9d31ba3f8
Udbhav <udbhav.sharma15@outlook.com>
2019-02-04 15:25:10 -0500

    Modified Train.py to take alpha as command line arg and print all the parameters

--- train.py
+++ train.py
@@ -8,10 +8,10 @@ from torch.utils.data import DataLoader
 from torch.autograd import Variable
 import torch.optim as optim
 import torch
-import torch.nn as nn
 import os
 
 parser = argparse.ArgumentParser(description='Train Super Resolution Models')
+parser.add_argument('--alpha', default=1.1, type=int)
 parser.add_argument('--crop_size', default=256, type=int, help='training images crop size')
 parser.add_argument('--upscale_factor', default=0, type=int, choices=[2, 4, 8],
                     help='super resolution upscale factor')
@@ -24,16 +24,17 @@ crop_size = opt.crop_size
 upscale_factor = opt.upscale_factor
 batch_size = 8
 num_epochs = 100
-alpha = 1.1
+alpha = opt.alpha
+train_data_path = 'data/train'
 
-train_set = DatasetFromFolder('data/train', crop_size, alpha=alpha)
+print("Training SR GAN with alpha: {}, batch size: {}, crop size: {}, train data: {} for epochs: {}".format(\
+    alpha, batch_size, crop_size, train_data_path, num_epochs))
 
+train_set = DatasetFromFolder(train_data_path, crop_size, alpha=alpha)
 train_loader = DataLoader(train_set, num_workers=0, batch_size=batch_size, shuffle=True)
 
 netG = Generator(upscale_factor)
 netD = Discriminator()
-#netG = nn.DataParallel(netG, [2,3])
-#netD = nn.DataParallel(netD, [2,3])
 
 gen_criterion = GeneratorLoss()
 
@@ -51,7 +52,6 @@ for epoch in range(1, num_epochs + 1):
 
     count = 1
     for lr_img, hr_img in train_loader:
-        #print(lr_img.dtype, hr_img.shape)
         lr_img = Variable(lr_img)
         hr_img = Variable(hr_img)
 
@@ -75,7 +75,6 @@ for epoch in range(1, num_epochs + 1):
         print("Training epoch {}, Batch_Num {}/{}, G_Loss {}, D_Loss {}".format(epoch, count, len(train_loader), g_loss, d_loss))
         count += 1
 
-    # print("Training epoch {}, G_Loss {}, D_Loss {}".format(epoch, g_loss, d_loss))
     if epoch % 1 == 0:
         torch.save(netG, 'save/netG_epoch_{}_{}.pth'.format(upscale_factor, epoch))
         torch.save(netD, 'save/netD_epoch_{}_{}.pth'.format(upscale_factor, epoch))
