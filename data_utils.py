f7b70300471cca23c65705ff26b91c2212e542cd
Udbhav <udbhav.sharma15@outlook.com>
2019-02-04 11:08:15 -0500

    Cleaning up data utils added .idea/ in gitignore

--- data_utils.py
+++ data_utils.py
@@ -1,19 +1,21 @@
 from os import listdir
 from os.path import join
 
-import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
-from torchvision.transforms import Compose, RandomCrop, ToTensor, CenterCrop, Resize, ToPILImage
+from torchvision.transforms import Compose, RandomCrop, ToTensor, Resize, ToPILImage
+
 
 def is_image(fname):
     return any(fname.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
+
 
 def train_hr_transform(crop_size):
     return Compose([
         RandomCrop(crop_size),
         ToTensor(),
     ])
+
 
 def train_lr_transform(crop_size, alpha):
     return Compose([
@@ -23,25 +25,29 @@ def train_lr_transform(crop_size, alpha):
         ToTensor()
     ])
 
+
 class DatasetFromFolder(Dataset):
-    def __init__(self, dataset_dir, crop_size, alpha = 1.1):
+    def __init__(self, dataset_dir, crop_size, alpha=1.1):
         super(DatasetFromFolder, self).__init__()
-        self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image(x)] #for _ in range(6)]
+        self.images = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image(x)]
         self.hr_transform = train_hr_transform(crop_size)
         self.lr_transform = train_lr_transform(crop_size, alpha)
         self.crop_size = crop_size
 
     def __getitem__(self, index):
-        img = Image.open(self.image_filenames[index])
+        img = Image.open(self.images[index])
         img = img.convert('RGB')
         width, height = img.size
+
+        # Resizing image such that width and height are multiples of crop_size
         width += self.crop_size - (width % self.crop_size)
         height += self.crop_size - (height % self.crop_size)
         img = img.resize((width, height))
+
         hr_image = self.hr_transform(img)
         lr_image = self.lr_transform(hr_image)
-        # print(lr_image.shape, lr_image.dtype)
+
         return lr_image, hr_image
 
     def __len__(self):
-        return len(self.image_filenames)
+        return len(self.images)
